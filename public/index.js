e034e9ebcbd312537386e69b84f4e9e88cb99e42
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-08-08 18:15:38 +0100

    Fetch config via ajax call. Support config provided in url.

--- public/index.js
+++ public/index.js
@@ -1,14 +1,32 @@
-var paths;
+var paths, fastlyHost, imgIxHost, mediaImgIxToken;
 
 function signImgIx(path) {
     return md5(`${mediaImgIxToken}${path}`);
 }
 
-const hostRadiosLeft = document.getElementsByName("originHost-left");
-const hostRadiosRight = document.getElementsByName("originHost-right");
-
+function fetchWithTimeout(url, options, timeout) {
+    return Promise.race([
+        fetch(url, options),
+        new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('timeout')), timeout)
+        )
+    ]);
+}
 
 const  isChrome = !!window.chrome && !!window.chrome.webstore;
+const pathList = document.getElementById("paths");
+const hostRadiosLeft = document.getElementsByName("originHost-left");
+const hostRadiosRight = document.getElementsByName("originHost-right");
+const hostMapping = [
+    {
+        name: "host-left",
+        element: hostRadiosLeft
+    },
+    {
+        name: "host-right",
+        element: hostRadiosRight
+    }
+]
 
 function switchOnHost(imgixValue, fastlyValue) {
     return {
@@ -54,25 +72,59 @@ function convertToKb(sizeInBytes) {
 
 var params = {}
 
-function getParams() {
+const elementsWithDirectParameterMapping = [ "dpr-left", "dpr-right", "quality-left", "quality-right"];
+
+function setHostRadioButton(buttonGroup, value) {
+    if (value === "imgix") {
+        buttonGroup[0].checked = true;
+        buttonGroup[1].checked = false;
+    } else {
+        buttonGroup[0].checked = false;
+        buttonGroup[1].checked = true;
+    }
+}
+
+function arrayLookup(array, key, defaultValue) {
+    const maybeItem = array.filter((element) => element[0] === key);
+    return maybeItem.length > 0 ? maybeItem[0][1] : defaultValue;
+}
+
+
+
+function getParams(configFromQueryString) {
     const queryString = window.location.search.substring(1);
-    console.log(queryString);
     const paramsArray = 
         queryString.split("&")
         .map((keyPair) => keyPair.split("="))
         .map((param) => param[0] === "paths" ? ["paths", param[1].split(",")] : param);
     paramsArray.forEach((param) => param[0] ? params[param[0]] = param[1] : "");
-    console.log(paramsArray);
-    paramsArray.forEach((param) => param[0] ? document.getElementById(param[0]).value = param[1] : undefined);
-    document.getElementById("paths").value = params.paths ? params.paths.join("\n") : "";
+    paramsArray
+        .filter((param) => elementsWithDirectParameterMapping.includes(param[0]))
+        .forEach((param) => param[0] ? document.getElementById(param[0]).value = param[1] : undefined);
+    console.log("setting paths", params.paths.join("\n"))
+    pathList.innerHTML = params.paths ? params.paths.join("\n") : "";
+    pathList.style.height = 'auto';
+    // set imgix/fastly radio buttons
+    hostMapping.forEach((host) => {
+        const value = arrayLookup(paramsArray, host.name, "imgix");
+        setHostRadioButton(host.element, value);
+        params[host.name] = value;
+    });
+
+    if (configFromQueryString) {
+            // try using query string params
+        fastlyHost = arrayLookup(paramsArray, "fastly-host", "missing");
+        imgIxHost = arrayLookup(paramsArray, "imgix-host", "missing");
+        mediaImgIxToken = arrayLookup(paramsArray, "imgix-token", "missing");
+    }
+
 }
-getParams();
+
 
 function paramsToQuerySting() {
     const paramsString = Object.entries(params)
         .map((param) => param[0] == "paths" ? `paths=${param[1].join(",")}` : `${param[0]}=${param[1]}`)
         .join("&");
-    console.log(paramsString);
     if (history.pushState) {
         const newurl = window.location.origin + window.location.pathname + `?${paramsString}`;
         window.history.pushState({path:newurl},'',newurl);
@@ -138,17 +190,30 @@ function reloadImageLists(){
     updateList("right", params);
 }
 
-const pathList = document.getElementById("paths");
 function updateImages() {
-    paths = pathList.value.replace(/https:\/\/media.guim.co.uk/g,"").split("\n");
+    paths = pathList.innerHTML.replace(/https:\/\/media.guim.co.uk/g,"").split("\n");
     params["paths"] = paths;
     paramsToQuerySting();
     reloadImageLists();
 }
 
+function updateHost(name, index) {
+    if (index == 0) {
+        params[name] = "imgix"
+    } else if (index === 1) {
+        params[name] = "fastly"
+    }
+}
 
-hostRadiosLeft.forEach((r) => r.addEventListener("input", (e) => updateImages()))
-hostRadiosRight.forEach((r) => r.addEventListener("input", (e) => updateImages()))
+hostMapping.forEach((host) => {
+    host.element.forEach((r, index) => r.addEventListener("input", (e) => {
+        updateHost(host.name, index);
+        updateImages();
+    }))
+})
+
+hostRadiosLeft.forEach((r, index) => r.addEventListener("input", (e) => updateImages()))
+hostRadiosRight.forEach((r, index) => r.addEventListener("input", (e) => updateImages()))
 
 pathList.addEventListener("input", function(e){updateImages();})
 
@@ -157,4 +222,22 @@ addListener("quality-left", "left", "quality");
 addListener("dpr-right", "right", "dpr");
 addListener("quality-right", "right", "quality");
 
-updateImages();
+
+fetchWithTimeout("http://image-comparison-tool.s3-website-eu-west-1.amazonaws.com/config.js", {}, timeout=1000)
+.then((resp) => {
+    if (resp.status === 200) {
+        resp.json().then((json) => {
+            imgIxHost = json.imgIxHost;
+            fastlyHost = json.fastlyHost;
+            mediaImgIxToken = json.mediaImgIxToken;
+            getParams(configFromQueryString = false);
+            updateImages();
+    
+        })
+    } else {
+        getParams(configFromQueryString = true);
+        updateImages();
+        return Promise.reject("Failed to fetch config - try setting fastly-host,imgix-host and imgix-token in query string.")
+    }
+}).catch((err)=>console.log(err))
+
