4509d2669da9075d19e59b19e2617dfd5c4b9fdf
Max Duval <max.duval@theguardian.com>
2023-07-12 15:28:35 +0100

    feat: link to repo

--- _site/routes/index.svelte
+++ _site/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-  <title>Mononykus – Deno + Svelte</title>
+  <title>Image Comparison Tool</title>
   <meta
     name="description"
     content="Deploy small websites built with Svelte with islands of interactivity."
@@ -13,8 +13,14 @@
 
 <body>
   <main>
-    <h1>Image Comparison tool</h1>
+    <h1>Image Comparison Tool</h1>
     <h2>Compare various image qualities and formats</h2>
+
+    <p>
+      Built from <a href="https://github.com/guardian/image-comparison-tool"
+        >guardian/image-comparison-tool</a
+      >
+    </p>
 
     <Images />
   </main>
