3bbaa179615540ad701b899f86855518d2f15449
Max Duval <max.duval@theguardian.com>
2024-06-18 15:28:56 +0100

    chore: bump deps
    also make it work on more deno versions

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # build project
-deno run -A https://deno.land/x/mononykus@0.6.1/src/build.ts
+deno run -A https://deno.land/x/mononykus@0.7.2/src/build.ts
 
 # upload project
 aws s3 sync build/ s3://image-comparison-tool/ --profile frontend
--- develop.sh
+++ develop.sh
@@ -1,2 +1,2 @@
 # develop project
-deno run -A https://deno.land/x/mononykus@0.6.1/src/build.ts --watch
+deno run -A https://deno.land/x/mononykus@0.7.2/src/build.ts --watch
