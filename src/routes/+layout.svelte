e52f816b39ff06184edf636c69d3a017c41d2e9f
Simon Adcock <simon.adcock@guardian.co.uk>
2026-01-12 21:52:25 +0000

    use sveltekit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Images from "../components/Images.island.svelte";
+  import Images from "../components/Images.svelte";
 </script>
 
 <svelte:head>
@@ -11,20 +11,18 @@
   <link rel="icon" href="/assets/favicon.svg" />
 </svelte:head>
 
-<body>
-  <main>
-    <h1>Image Comparison Tool</h1>
-    <h2>Compare various image qualities and formats</h2>
+<main>
+  <h1>Image Comparison Tool</h1>
+  <h2>Compare various image qualities and formats</h2>
 
-    <p>
-      Built from <a href="https://github.com/guardian/image-comparison-tool"
-        >guardian/image-comparison-tool</a
-      >
-    </p>
+  <p>
+    Built from <a href="https://github.com/guardian/image-comparison-tool"
+      >guardian/image-comparison-tool</a
+    >
+  </p>
 
-    <Images />
-  </main>
-</body>
+  <Images />
+</main>
 
 <style>
   :global(:root) {
